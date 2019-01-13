562bc7e629d596612da380e48d06ca5d9b58bb37
Matthew Collins <matthew.collins1991@yahoo.com>
2019-01-13 22:36:57 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Mr. Chocolate", 12 , "Male", "black", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Rowdy", 9 , "Male", "black", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Tabitha", 6 , "Female", "black", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Sergeant Brown", 6 , "Male", "Brown", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Melissa", 13 , "female", "black", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Grinch", 5 , "Male", "black", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES ("Wendy", 6 , "female", "black", "Angry", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament,alive) VALUES (NULL, 18 , "female", "black", "Angry", 1);
